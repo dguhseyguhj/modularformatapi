22f218bc0dcb14e6639b4a324473abe3b89a5265
dguhseyguhj <csjcrazybaby456@hotmail.com>
2024-05-09 21:20:29 +0700

    feat project commit

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
-const person = { name: "Alice", age: 30 };
-if (person.hasOwnProperty("age")) {
-  console.log("The person has an age property");
+function decodeString(s) {
+  const stack = [];
+  for (const char of s) {
+    if (char !== "]") stack.push(char);
+    else {
+      let str = "";
+      while (stack.length && stack[stack.length - 1] !== "[")
+        str = stack.pop() + str;
+      stack.pop();
+      let num = "";
+      while (stack.length && !isNaN(stack[stack.length - 1]))
+        num = stack.pop() + num;
+      stack.push(str.repeat(parseInt(num)));
+    }
+  }
+  return stack.join("");
 }
